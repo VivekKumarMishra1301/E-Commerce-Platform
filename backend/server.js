5af7a62bc5d482632803eb1fa8ca28f0a80da377
Vivek <vivekkmishra1301@gmail.com>
2024-01-09 21:45:04 +0530

    Categories CRUD API

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import morgan from "morgan";
 import cors from 'cors'
 import authRoute from './routes/authRoute.js';
-
+import categoryRoutes from './routes/categoryRoutes.js';
 dotenv.config();
 connectDB();
 const app = express();
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/v1/auth', authRoute);
-
+app.use('/api/v1/category',categoryRoutes);
 
 
 
